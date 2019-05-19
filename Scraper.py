841130802c917eb25404f260603918905b9d62a0
Mohammedwich <45858931+Mohammedwich@users.noreply.github.com>
2019-05-19 14:40:11 -0500

    Initial commit

---
+++ Scraper.py
@@ -0,0 +1,16 @@
+# The first step is to import the requests library and download the Boone County webpage.
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://books.toscrape.com/"
+response = requests.get(url)
+print("Response code: {}".format(response.status_code))
+html_text = response.content
+# print(html_text)
+
+soup = BeautifulSoup(html_text, features="html.parser")
+titles = []
+for title in soup.find_all('a', title=True):
+    titles.append(title)
+print(titles)
+
